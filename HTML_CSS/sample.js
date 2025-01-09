18b2e029e3ab04b7dfb0cab85b09f9fe918b410d
HariDharsaun <haridharsaun18personal@gmail.com>
2025-01-09 23:13:50 +0530

    added js files day 4

---
+++ HTML_CSS/sample.js
@@ -0,0 +1,34 @@
+var a = 10
+let char = 'a'
+let str = "Hari"
+let bool = true
+const pi = 3.14
+
+//primitive datatypes
+console.log(a);
+console.log(char)
+console.log(str)
+console.log(bool);
+console.log(pi)
+
+//checking the type of the datatype
+console.log(typeof(char))
+
+//secondary data types
+
+//Object
+let array = ['a',10,true,[20,30],"sri eshwar",{1:"hari"}]
+let obj = {firstname:"hari",lastname:"dharsaun",age:[10,20,30]}
+let obj2 = new Map()
+console.log(array[5][1])
+console.log(obj.lastname)
+console.log(obj["age"][0])
+obj2.firstname = "Hari"
+obj2.set("lastname","Dharsaun")
+console.log(obj2)
+console.log(obj2.size)
+
+//Set
+
+let set = new Set()
+console
